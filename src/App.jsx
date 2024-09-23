65e3279295c4ad2abb04c19d341ee29d800eba2d
Sam. <swayams24@gmail.com>
2024-09-24 01:51:33 +1000

    Code refacto

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,35 @@
-import { useState } from "react";
-import Api from "./components/api";
+import React, { useEffect, useState } from "react";
 import Search from "./components/Search";
+import Output from "./components/Output";
 import { apiKey } from "./components/config.js";
 import "./App.css";
 
 function App() {
   const [city, setCity] = useState("Sydney");
+  const [readings, setReadings] = useState(null);
+  const currentDate = new Date().toISOString().slice(0, 10);
+  const forecastDays = new Date(Date.now() + 7 * 24 * 60 * 60 * 1000)
+    .toISOString()
+    .slice(0, 10);
+  const URL = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline`;
+
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch(
+        `${URL}/${city}/${currentDate}/${forecastDays}?unitGroup=metric&key=${apiKey}`
+      );
+      const data = await response.json();
+      setReadings(data);
+    }
+    fetchData();
+  }, [city]);
+
+  console.log(readings);
+
   return (
     <>
       <Search setCity={setCity} />
-      <Api city={city} apiKey={apiKey} />
+      <Output readings={readings} />
     </>
   );
 }
