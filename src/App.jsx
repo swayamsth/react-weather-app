dc7de180bd42fb8e12075f3484fe155cc5b1aba0
Sam. <swayams24@gmail.com>
2024-09-24 03:11:12 +1000

    Gave up on dynamic background according to timezone

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,6 @@ import "./App.css";
 function App() {
   const [city, setCity] = useState("Sydney");
   const [readings, setReadings] = useState(null);
-  const [isDayTime, setIsDayTime] = useState(true);
   const currentDate = new Date().toISOString().slice(0, 10);
   const forecastDays = new Date(Date.now() + 7 * 24 * 60 * 60 * 1000)
     .toISOString()
@@ -29,26 +28,11 @@ function App() {
       );
       const data = await response.json();
       setReadings(data);
-
-      const sunrise = data.days[0].sunrise.slice(0, 2);
-      const sunset = data.days[0].sunset.slice(0, 2);
-
-      setIsDayTime(currentTime >= sunrise && currentTime <= sunset);
     }
     fetchData();
   }, [city]);
 
-  useEffect(() => {
-    const body = document.body;
-    const dayGradient = "linear-gradient(to top, #00d4ff, #6dd5fa, #2980b9);";
-    const nightGradient = "linear-gradient(to top, #000428, #004e92)";
-
-    body.style.background = isDayTime ? dayGradient : nightGradient;
-
-    return () => {
-      body.style.background = "";
-    };
-  }, [isDayTime]);
+  console.log(readings);
 
   if (!readings) {
     return <div>Loading...</div>;
